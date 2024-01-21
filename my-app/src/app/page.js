eeb95a7ec6d5bd33970a7be314c636f9de1a97e9
DevMayani <106169881+DevMayani@users.noreply.github.com>
2024-01-21 19:43:11 +0100

    add

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,5 +1,7 @@
+import Footer from "@/Components/Footer/footer";
 import Header from "@/Components/Header/header";
 import Navbar from "@/Components/Navbar/navbar";
+import Action from '@/Components/ActionComp/action'
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +9,8 @@ export default function Home() {
    <main>
     <Header />
     <Navbar />
+    <Action />
+    <Footer />
    </main>
   );
 }
