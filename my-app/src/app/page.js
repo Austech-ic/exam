2dfcf0677752e109224c00f1dc89ec3c14578517
DevMayani <106169881+DevMayani@users.noreply.github.com>
2024-01-21 17:38:19 +0100

    add new

---
+++ my-app/src/app/page.js
@@ -0,0 +1,12 @@
+import Header from "@/Components/Header/header";
+import Navbar from "@/Components/Navbar/navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <main>
+    <Header />
+    <Navbar />
+   </main>
+  );
+}
